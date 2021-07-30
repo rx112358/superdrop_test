b3134d02fb29696c4275323ea85548d23ab3860a
rx112358 <53977169+rx112358@users.noreply.github.com>
2021-07-30 12:44:07 +0400

    Update settings.py

--- search/settings.py
+++ search/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
